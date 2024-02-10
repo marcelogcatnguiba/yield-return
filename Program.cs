112c69abeea3a50b9f01d3437f2e8f022732250c
Marcelo <marcelogomes.0104@gmail.com>
2024-02-10 08:10:21 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿foreach (var n in SortearNumero())
+{
+    System.Console.Write($"{n}\t");
+}
+
+static IEnumerable<int> SortearNumero()
+{
+    var randonInt = new Random();
+    for (int i = 0; i < 6; i++)
+    {
+        yield return randonInt.Next(1, 60);
+    }
+}
